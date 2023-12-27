7678078b9d88817841775a2a895099e5f1db0df6
hp <nsheoran2005@gmail.com>
2023-12-27 21:47:23 +0530

    method create

--- lib/page-1/webinar_past_page.dart
+++ lib/page-1/webinar_past_page.dart
@@ -38,6 +38,7 @@ class CustomWebinarCard extends StatelessWidget {
       required this.bannerImg,
       required this.title,
       required this.showDuration});
+
   final bool isRegisterNow;
   final String btnTitle;
   final String title;
@@ -49,109 +50,131 @@ class CustomWebinarCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 8,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Container(
-          height: 190,
-          // width: 390,
-          decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(10),
-              image: DecorationImage(
-                  image: AssetImage(bannerImg), fit: BoxFit.cover)),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(10, 8, 20, 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: SafeGoogleFont(
-                  "Inter",
-                  fontSize: 17,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              const SizedBox(height: 8),
-              Row(
+    return SizedBox(
+      height: MediaQuery.of(context).size.height *0.6,
+      child: PageView(
+        children: [
+          cardView(context),
+          cardView(context),
+          cardView(context),
+        ],
+      ),
+    );
+  }
+
+  Widget cardView(BuildContext context) {
+    return Column(
+      children: [
+        Card(
+          elevation: 8,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Container(
+              height: 208,
+              // width: 390,
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                      image: AssetImage(bannerImg), fit: BoxFit.cover)),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 8, 20, 20),
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        time,
-                        style: SafeGoogleFont(
-                          "Inter",
-                          fontSize: 12,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 3,
-                      ),
-                      Text(
-                        showDuration
-                            ? "Duration : $duration"
-                            : "Participants - $participants",
-                        style: SafeGoogleFont(
-                          "Inter",
-                          fontSize: 12,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                    ],
-                  ),
-                  customEnrollButton(
-                      onPresssed: () {}, title: "Free Enroll", context: context)
-                ],
-              ),
-              const SizedBox(
-                height: 23,
-              ),
-              Container(
-                height: 1,
-                width: double.infinity,
-                color: const Color(0xffAFAFAF),
-              ),
-              const SizedBox(
-                height: 22,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CircleAvatar(
-                    backgroundColor: const Color(0xff7F90F7),
-                    child: Center(
-                      child: Image.asset(
-                        "assets/page-1/images/group-38-oFX.png",
-                        width: 17,
-                        height: 17,
-                        color: Colors.white,
-                      ),
+                  Text(
+                    title,
+                    style: SafeGoogleFont(
+                      "Inter",
+                      fontSize: 17,
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
-                  customRegisterNowBtn(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const WebinarDetailsPage(),
+                  const SizedBox(height: 4),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            time,
+                            style: SafeGoogleFont(
+                              "Inter",
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                            ),
                           ),
-                        );
-                      },
-                      title: btnTitle,
-                      isRegisterNow: isRegisterNow)
+                          const SizedBox(
+                            height: 3,
+                          ),
+                          Text(
+                            showDuration
+                                ? "Duration : $duration"
+                                : "Participants - $participants",
+                            style: SafeGoogleFont(
+                              "Inter",
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                      customEnrollButton(
+                          onPresssed: () {},
+                          title: "Free Enroll",
+                          context: context)
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    height: 1,
+                    width: double.infinity,
+                    color: const Color(0xffAFAFAF),
+                  ),
+                  const SizedBox(
+                    height: 14,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: const Color(0xff7F90F7),
+                        child: Center(
+                          child: Image.asset(
+                            "assets/page-1/images/group-38-oFX.png",
+                            width: 17,
+                            height: 17,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      customRegisterNowBtn(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    const WebinarDetailsPage(),
+                              ),
+                            );
+                          },
+                          title: btnTitle,
+                          isRegisterNow: isRegisterNow)
+                    ],
+                  )
                 ],
-              )
-            ],
-          ),
-        )
-      ]),
+              ),
+            )
+          ]),
+        ),
+      ],
     );
   }
 }
